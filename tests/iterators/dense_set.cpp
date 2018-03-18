ab9fe63986c7e2cb17cbe44c2a6f08315a950aa0
Akshay Nanavati <akshay.nanavati1@gmail.com>
2018-03-17 17:09:13 -0700

    Test on 32 bit ints so we have positive lookups

--- tests/iterators/dense_set.cpp
+++ tests/iterators/dense_set.cpp
@@ -12,10 +12,10 @@ constexpr size_t kMinSize = 1 << 5;
 constexpr size_t kMaxSize = 1 << 15;
 constexpr size_t kLookups = 1 << 10;
 
-std::pair<DenseSet<uint64_t>, std::unordered_set<uint64_t>> testCase(size_t n) {
-  static std::mt19937_64 rng(time(NULL));
-  DenseSet<uint64_t> ds;
-  std::unordered_set<uint64_t> us;
+std::pair<DenseSet<uint32_t>, std::unordered_set<uint32_t>> testCase(size_t n) {
+  static std::mt19937 rng(time(NULL));
+  DenseSet<uint32_t> ds;
+  std::unordered_set<uint32_t> us;
   for (size_t i = 0; i < n; ++i) {
     auto val = rng();
     ds.insert(val);
@@ -25,7 +25,7 @@ std::pair<DenseSet<uint64_t>, std::unordered_set<uint64_t>> testCase(size_t n) {
 }
 
 TEST(DenseSet, Lookup) {
-  static std::mt19937_64 rng(time(NULL) / 2);
+  static std::mt19937 rng(time(NULL) / 2);
   for (size_t n = kMinSize; n <= kMaxSize; n <<= 1) {
     auto [dset, uset] = testCase(n);
     ASSERT_EQ(dset.size(), uset.size());

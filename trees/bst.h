e2d4d9aa97d8923edb26db66d25580a16fa59b57
Akshay Nanavati <akshay.nanavati1@gmail.com>
2018-03-19 21:22:22 -0700

    Pull out common bst components

---
+++ trees/bst.h
@@ -0,0 +1,109 @@
+#include <memory>
+
+namespace falcon {
+template <template <typename, typename> class Node, class Key, class Val>
+class BST {
+protected:
+  std::unique_ptr<Node<Key, Val>> root_ = nullptr;
+  size_t size_ = 0;
+
+  void rotateLeft(Node<Key, Val> *node) {
+    auto parent = node->parent;
+    auto nodeUnique = std::move(parent->right);
+    parent->right = std::move(node->left);
+    if (parent->right) {
+      parent->right->parent = parent;
+    }
+    auto grandparent = parent->parent;
+    if (grandparent == nullptr) {
+      node->left = std::move(root_);
+      node->left->parent = node;
+      root_ = std::move(nodeUnique);
+      root_->parent = nullptr;
+    } else if (parent->isLeft()) {
+      node->left = std::move(grandparent->left);
+      node->left->parent = node;
+      grandparent->left = std::move(nodeUnique);
+      grandparent->left->parent = grandparent;
+    } else {
+      node->left = std::move(grandparent->right);
+      node->left->parent = node;
+      grandparent->right = std::move(nodeUnique);
+      grandparent->right->parent = grandparent;
+    }
+  }
+
+  void rotateRight(Node<Key, Val> *node) {
+    auto parent = node->parent;
+    auto nodeUnique = std::move(parent->left);
+    parent->left = std::move(node->right);
+    if (parent->left) {
+      parent->left->parent = parent;
+    }
+    auto grandparent = parent->parent;
+    if (grandparent == nullptr) {
+      node->right = std::move(root_);
+      node->right->parent = node;
+      root_ = std::move(nodeUnique);
+      node->parent = nullptr;
+    } else if (parent->isLeft()) {
+      node->right = std::move(grandparent->left);
+      node->right->parent = node;
+      grandparent->left = std::move(nodeUnique);
+      grandparent->left->parent = grandparent;
+    } else {
+      node->right = std::move(grandparent->right);
+      node->right->parent = node;
+      grandparent->right = std::move(nodeUnique);
+      grandparent->right->parent = grandparent;
+    }
+  }
+
+  Node<Key, Val> *insert(Key key, Val val) {
+    auto cur = root_.get();
+    while (true) {
+      if (key == cur->key) {
+        cur->val = std::forward<Val>(val);
+        return nullptr;
+      } else if (key < cur->key) {
+        if (cur->left) {
+          cur = cur->left.get();
+        } else {
+          cur->left =
+              std::make_unique<Node<Key, Val>>(std::move(key), std::move(val));
+          cur->left->parent = cur;
+          ++size_;
+          return cur->left.get();
+        }
+      } else {
+        if (cur->right) {
+          cur = cur->right.get();
+        } else {
+          cur->right = std::make_unique<Node<Key, Val>>(std::forward<Key>(key),
+                                                        std::forward<Val>(val));
+          cur->right->parent = cur;
+          ++size_;
+          return cur->right.get();
+        }
+      }
+    }
+  }
+
+public:
+  size_t size() const { return size_; }
+
+  Val *lookup(const Key &key) const {
+    auto cur = root_.get();
+    while (cur) {
+      if (key == cur->key) {
+        return &cur->val;
+      } else if (key < cur->key) {
+        cur = cur->left.get();
+      } else {
+        cur = cur->right.get();
+      }
+    }
+    return nullptr;
+  }
+};
+} // namespace falcon
--- trees/redblack.h
+++ trees/redblack.h
@@ -1,5 +1,6 @@
 #include <memory>
 
+#include "trees/bst.h"
 #include "utils/utils.h"
 
 namespace {
@@ -37,63 +38,12 @@ template <class Key, class Val> struct RBNode {
 } // namespace
 
 namespace falcon {
-template <class Key, class Val> class RBTree {
-  std::unique_ptr<RBNode<Key, Val>> root_ = nullptr;
-  size_t size_ = 0;
-
-  void rotateLeft(RBNode<Key, Val> *node) {
-    auto parent = node->parent;
-    auto nodeUnique = std::move(parent->right);
-    parent->right = std::move(node->left);
-    if (parent->right) {
-      parent->right->parent = parent;
-    }
-    auto grandparent = parent->parent;
-    if (grandparent == nullptr) {
-      node->left = std::move(root_);
-      node->left->parent = node;
-      root_ = std::move(nodeUnique);
-      root_->parent = nullptr;
-    } else if (parent->isLeft()) {
-      node->left = std::move(grandparent->left);
-      node->left->parent = node;
-      grandparent->left = std::move(nodeUnique);
-      grandparent->left->parent = grandparent;
-    } else {
-      node->left = std::move(grandparent->right);
-      node->left->parent = node;
-      grandparent->right = std::move(nodeUnique);
-      grandparent->right->parent = grandparent;
-    }
-  }
-
-  void rotateRight(RBNode<Key, Val> *node) {
-    auto parent = node->parent;
-    auto nodeUnique = std::move(parent->left);
-    parent->left = std::move(node->right);
-    if (parent->left) {
-      parent->left->parent = parent;
-    }
-    auto grandparent = parent->parent;
-    if (grandparent == nullptr) {
-      node->right = std::move(root_);
-      node->right->parent = node;
-      root_ = std::move(nodeUnique);
-      node->parent = nullptr;
-    } else if (parent->isLeft()) {
-      node->right = std::move(grandparent->left);
-      node->right->parent = node;
-      grandparent->left = std::move(nodeUnique);
-      grandparent->left->parent = grandparent;
-    } else {
-      node->right = std::move(grandparent->right);
-      node->right->parent = node;
-      grandparent->right = std::move(nodeUnique);
-      grandparent->right->parent = grandparent;
-    }
-  }
-
+template <class Key, class Val> class RBTree : public BST<RBNode, Key, Val> {
   void rebalance(RBNode<Key, Val> *node) {
+    if (node == nullptr) {
+      return;
+    }
+
     auto parent = node->parent;
     if (parent == nullptr) {
       node->color = Color::Black;
@@ -114,17 +64,17 @@ template <class Key, class Val> class RBTree {
     }
 
     if (grandparent->left && node == grandparent->left->right.get()) {
-      rotateLeft(node);
+      BST<RBNode, Key, Val>::rotateLeft(node);
       std::swap(node, parent);
     } else if (grandparent->right && node == grandparent->right->left.get()) {
-      rotateRight(node);
+      BST<RBNode, Key, Val>::rotateRight(node);
       std::swap(node, parent);
     }
 
     if (node->isLeft()) {
-      rotateRight(parent);
+      BST<RBNode, Key, Val>::rotateRight(parent);
     } else {
-      rotateLeft(parent);
+      BST<RBNode, Key, Val>::rotateLeft(parent);
     }
     parent->color = Color::Black;
     grandparent->color = Color::Red;
@@ -138,59 +88,16 @@ template <class Key, class Val> class RBTree {
   }
 
 public:
-  size_t size() const { return size_; }
-
   void insert(Key key, Val val) {
-    auto cur = root_.get();
-    if (UNLIKELY(cur == nullptr)) {
-      root_ = std::make_unique<RBNode<Key, Val>>(std::forward<Key>(key),
-                                                 std::forward<Val>(val));
-      root_->color = Color::Black;
-      ++size_;
+    if (BST<RBNode, Key, Val>::root_ == nullptr) {
+      BST<RBNode, Key, Val>::root_ = std::make_unique<RBNode<Key, Val>>(
+          std::forward<Key>(key), std::forward<Val>(val));
+      BST<RBNode, Key, Val>::root_->color = Color::Black;
+      ++BST<RBNode, Key, Val>::size_;
       return;
     }
-    while (true) {
-      if (key == cur->key) {
-        cur->val = std::forward<Val>(val);
-        return;
-      } else if (key < cur->key) {
-        if (cur->left) {
-          cur = cur->left.get();
-        } else {
-          cur->left = std::make_unique<RBNode<Key, Val>>(
-              std::forward<Key>(key), std::forward<Val>(val));
-          cur->left->parent = cur;
-          rebalance(cur->left.get());
-          ++size_;
-          return;
-        }
-      } else {
-        if (cur->right) {
-          cur = cur->right.get();
-        } else {
-          cur->right = std::make_unique<RBNode<Key, Val>>(
-              std::forward<Key>(key), std::forward<Val>(val));
-          cur->right->parent = cur;
-          rebalance(cur->right.get());
-          ++size_;
-          return;
-        }
-      }
-    }
-  }
 
-  Val *lookup(const Key &key) const {
-    auto cur = root_.get();
-    while (cur) {
-      if (key == cur->key) {
-        return &cur->val;
-      } else if (key < cur->key) {
-        cur = cur->left.get();
-      } else {
-        cur = cur->right.get();
-      }
-    }
-    return nullptr;
+    rebalance(BST<RBNode, Key, Val>::insert(std::move(key), std::move(val)));
   }
 };
 } // namespace falcon

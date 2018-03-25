4dd4a09a0d421b5ae953d1e5d9f8bb829b174527
Akshay Nanavati <akshay.nanavati1@gmail.com>
2018-03-24 22:09:11 -0700

    Persist hll to disk

--- include/falcon/sets/hll.h
+++ include/falcon/sets/hll.h
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <array>
 #include <cmath>
+#include <fstream>
+#include <sstream>
 
 #include "falcon/utils/utils.h"
 
@@ -28,7 +30,7 @@ class HyperLogLog {
   Hash<Key> hasher_;
   std::array<uint8_t, M> registers_;
 
-  uint8_t countTrailingZeroes(uint64_t w) {
+  uint8_t countTrailingZeroes(uint64_t w) const {
     if (UNLIKELY(w == 0)) {
       return 64 - LogM;
     }
@@ -38,6 +40,21 @@ class HyperLogLog {
 public:
   explicit HyperLogLog() { reset(); };
   explicit HyperLogLog(long double alpha) : alpha_(alpha) { reset(); }
+  static HyperLogLog<Key, Hash, LogM> readFromDisk(const std::string &fname) {
+    std::ifstream infile(fname, std::ios::binary);
+    char logm;
+    infile.read(&logm, 1);
+    if (UNLIKELY(logm != LogM)) {
+      std::stringstream ss;
+      ss << "LogM from file " << logm << " does not match LogM " << LogM;
+      throw std::runtime_error(ss.str());
+    }
+    long double alpha;
+    infile.read(reinterpret_cast<char *>(&alpha), sizeof(long double));
+    HyperLogLog<Key, Hash, LogM> hll(alpha);
+    infile.read(hll.registers_.data(), hll.registers_.size());
+    return hll;
+  }
 
   void insert(const Key &key) {
     uint64_t hash = hasher_(key);
@@ -47,9 +64,20 @@ public:
         std::max(registers_[registerIdx], (uint8_t)countTrailingZeroes(w));
   }
 
-  size_t count() {
+  void setAlpha(long double alpha) { alpha_ = alpha; }
+
+  size_t count() const {
     long double z = detail::harmonicMean(registers_);
     return z * (M << LogM) * alpha_;
+  }
+
+  void saveToDisk(const std::string &fname) const {
+    std::ofstream of(fname, std::ios::binary);
+    of.put(static_cast<char>(LogM));
+    of.write(reinterpret_cast<const char *>(&alpha_), sizeof(long double));
+    of.write(reinterpret_cast<const char *>(registers_.data()),
+             registers_.size());
+    of.flush();
   }
 
   void reset() { std::fill(registers_.begin(), registers_.end(), 0); }
